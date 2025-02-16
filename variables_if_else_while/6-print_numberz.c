4f8190fa28a7879b3d33ccbe38c12c14c3d5440c
Muhannad-09 <muhannadgsgs@gmail.com>
2025-02-16 10:57:40 +0000

    Numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*
+ * File: 6-print_numberz.c
+ * Created by: Muhannad Gsgs 10722@holbertonstudents.com
+ */
+
+/**
+ * main - prints numbers without using char variable
+ * Return: 0
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		putchar(number + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
