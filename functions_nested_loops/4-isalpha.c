055219f0b57b121035a46ff7d9ca27a8246bf88c
Muhannad-09 <muhannadgsgs@gmail.com>
2025-02-17 10:52:25 +0000

    isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/*
+ * File - 4-isalpha.c
+ * Created by: Muhannad Gsgs 10722@holbertonstudents.com
+ */
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: The character to be checked
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
