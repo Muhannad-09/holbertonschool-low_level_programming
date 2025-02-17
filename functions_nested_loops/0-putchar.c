ad4f7aa7cb1e5e433822113212ceac9f9e68c2be
Muhannad-09 <muhannadgsgs@gmail.com>
2025-02-17 10:02:30 +0000

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/*
+ * File - 0-putchar.c
+ * Created by: Muhannad Gsgs 10722@holbertonstudents.com
+ */
+
+/**
+ * main - prints _putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
