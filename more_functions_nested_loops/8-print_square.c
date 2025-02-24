579df89f68e7e5cbb2f811ae6ddc3d4031924b13
Muhannad-09 <muhannadgsgs@gmail.com>
2025-02-24 14:42:07 +0000

    8.You are so much sunshine in every square inch

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/*
+ * File - 8-print_square.c
+ * Created by: Muhannad Gsgs 10722@holbertonstudents.com
+ */
+
+
+/**
+ * print_square - prints a square, followed by a new line
+ * @size: size of the square
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
