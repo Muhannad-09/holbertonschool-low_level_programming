7fc55e33dadc9352091ded07668d31ab4c5957af
Muhannad-09 <muhannadgsgs@gmail.com>
2025-02-24 15:05:53 +0000

    10.Triangles

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/*
+ * File - 10-print_triangle.c
+ * Created by: Muhannad Gsgs 10722@holbertonstudents.com
+ */
+
+/**
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size - i; j > 0; j--)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k < i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
