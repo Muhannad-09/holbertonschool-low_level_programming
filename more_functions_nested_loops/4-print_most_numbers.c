50d6300cd68495bc5c2081726b9496ff8b30bd5d
Muhannad-09 <muhannadgsgs@gmail.com>
2025-02-24 13:06:31 +0000

    4.I believe in numbers and signs

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/*
+ * File - 4-print_most _numbers.c
+ * Created by: Muhannad Gsgs 10722@holbertonstudents.com
+ */
+
+/**
+ * print_most_numbers - prints numbers without (2, 4) followed by new line
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+		_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
