bb236bfb193f325fc34052a5ff564bf12a7d660c
Muhannad-09 <muhannadgsgs@gmail.com>
2025-02-25 14:18:00 +0000

    Great leaders are willing to sacrifice the numbers to save the people

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/*
+ * File - 100-atoi.c
+ * Created by: Muhannad Gsgs 10722@holbertonstudents.com
+ */
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: pointer to the string to be converted
+ *
+ * Return: the converted integer
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		else if (s[i] == '+')
+		{
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			break;
+		}
+		i++;
+	}
+
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return (result * sign);
+}
