1c661599aad215c9dbf3591d4b72e2c077c8dcbd
Muhannad-09 <muhannadgsgs@gmail.com>
2025-02-25 09:26:29 +0000

    0.98 Battery st.

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/*
+ * File - 0-reset_to_98.c
+ * Created by: Muhannad Gsgs 10722@holbertonstudents.com
+ */
+
+/**
+ * reset_to_98 - updates the value of the variable
+ *               the pointer points to to 98
+ * @n: pointer to the variable to update
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
