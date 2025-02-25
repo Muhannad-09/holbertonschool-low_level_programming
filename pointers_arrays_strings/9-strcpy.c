3e48126d4e7e8fa5d6dc3fd65ff1f550c467b9af
Muhannad-09 <muhannadgsgs@gmail.com>
2025-02-25 13:20:20 +0000

    9.strcpy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/*
+ * File - 9-strcpy.c
+ * Created by: Muhannad Gsgs 10722@holbertonstudents.com
+ */
+
+/**
+ * _strcpy-copies the string pointed to by src, including terminating null byte
+ * @dest: pointer to the buffer where the string is copied
+ * @src: pointer to the source string
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
