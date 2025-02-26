6219a1c9f9c5bb1e35f4a04a241c6ca1b2cd8158
Muhannad-09 <muhannadgsgs@gmail.com>
2025-02-26 14:16:41 +0000

    2.strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/*
+ * File: 2-strncpy.c
+ * Author: Muhannad Gsgs
+ */
+
+/**
+ * _strncpy - Copies at most n bytes of the source string to the destination
+ *            string. This function takes two strings, `dest` and `src`, and
+ *            copies up to `n` bytes from `src` to `dest`. If `src` is less
+ *            than `n` bytes long, the remainder of `dest` will be padded with
+ *            null bytes. The resulting string is not null-terminated if `n`
+ *            is reached.
+ *
+ * @dest: The destination string.
+ * @src: The source string.
+ * @n: The number of bytes to copy from src.
+ *
+ * Return: A pointer to the resulting string @dest.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
