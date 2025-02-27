2ba6a45521b73a7bb956a969e249b1e4e3c00f07
Muhannad-09 <muhannadgsgs@gmail.com>
2025-02-27 08:17:13 +0000

    6.Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/*
+ * File: source_6-file.c
+ * Author: Muhannad Gsgs
+ */
+
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to modify.
+ *
+ * Return: A pointer to the modified string.
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	char separators[] = " \t\n,;.!?\"(){}";
+	int j;
+
+	if (str[i] >= 'a' && str[i] <= 'z')
+	{
+		str[i] = str[i] - 32;
+	}
+
+	while (str[i] != '\0')
+	{
+		for (j = 0; separators[j] != '\0'; j++)
+		{
+			if (str[i] == separators[j] && str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			{
+				str[i + 1] = str[i + 1] - 32;
+				break;
+			}
+		}
+		i++;
+	}
+
+	return (str);
+}
