ec533624a50efd8810ac3378e1765d552a019dcf
Muhannad-09 <muhannadgsgs@gmail.com>
2025-02-27 10:31:54 +0000

    102

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -2,12 +2,12 @@
 
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+    int n;
+    int a[5];
+    int *p;
 
-	a[2] = 1024;
-	p = &n;
+    a[2] = 1024;
+    p = &n;
 
     /*
      * write your line of code here...
@@ -19,14 +19,9 @@ int main(void)
      * - you are not allowed to code anything else than the
      *   line of code at the expected line
      */
-	*(p + 5) = 98;
+    *(p + 5) = 98;
     /* ...so that this prints 98\n */
-
-    /**
-     * main - Entry point of the program
-     *
-     * Return: Always 0 (Success)
-     */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+    printf("a[2] = %d\n", a[2]);
+    return (0);
 }
+
