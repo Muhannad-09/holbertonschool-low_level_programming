6025ec8b80e81b52ece3ebcd6b461597674a496f
Muhannad-09 <muhannadgsgs@gmail.com>
2025-03-11 11:24:20 +0000

    new_dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,11 +1,45 @@
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
 /*
  * File: source_4-file.c
  * Author: Muhannad Gsgs
  */
+
+/**
+ * _strlength - Calculates the length of a string
+ * @str: The string to measure
+ *
+ * Return: The length of the string (excluding the null terminator)
+ */
+
+int _strlength(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	return (len);
+}
+
+/**
+ * _strcopy - Copies a string to a newly allocated memory space
+ * @dest: Destination for the copy
+ * @src: Source string to copy
+ *
+ * Return: Pointer to the copied string
+ */
+char *_strcopy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+
+	return (dest);
+}
 
 /**
  * new_dog - Creates a new dog
@@ -27,23 +61,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 
 	/* Allocate memory for and copy the name */
-	name_copy = malloc(strlen(name) + 1);
+	name_copy = malloc(_strlength(name) + 1);
 	if (name_copy == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-	strcpy(name_copy, name);
+	_strcopy(name_copy, name);
 
 	/* Allocate memory for and copy the owner */
-	owner_copy = malloc(strlen(owner) + 1);
+	owner_copy = malloc(_strlength(owner) + 1);
 	if (owner_copy == NULL)
 	{
 		free(name_copy);
 		free(new_dog);
 		return (NULL);
 	}
-	strcpy(owner_copy, owner);
+	_strcopy(owner_copy, owner);
 
 	/* Initialize the new dog's members */
 	new_dog->name = name_copy;
