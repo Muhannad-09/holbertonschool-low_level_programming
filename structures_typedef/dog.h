f3104e059e18dde2daef3576999f7a069cbf4ee1
Muhannad-09 <muhannadgsgs@gmail.com>
2025-03-11 10:23:34 +0000

    0.Poppy

---
+++ structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/*
+ * File: source_0-file.c
+ * Author: Muhannad Gsgs
+ */
+
+/**
+ * struct dog - A new type describing a dog
+ * @name: Name of the dog (type char *)
+ * @age: Age of the dog (type float)
+ * @owner: Owner of the dog (type char *)
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
