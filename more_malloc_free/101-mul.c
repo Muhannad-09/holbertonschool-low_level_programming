f520333e23bd036c2722c67381db0c1221e46be4
Muhannad-09 <muhannadgsgs@gmail.com>
2025-03-12 10:55:35 +0000

    101.mul

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,133 +1,88 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
-
-/*
- * File: source_101-file.c
- * Author: Muhannad Gsgs
- */
 
 /**
- * _isdigit - Checks if a string is composed of only digits.
- * @s: The string to check.
- *
- * Return: 1 if the string is all digits, 0 otherwise.
+ * _puts - prints a string, followed by a new line
+ * @str: pointer to the string to print
+ * Return: void
  */
 
-int _isdigit(char *s)
+void _puts(char *str)
 {
-	while (*s)
+	int i = 0;
+
+	while (str[i])
 	{
-		if (*s < '0' || *s > '9')
-			return (0);
-		s++;
-	}
-	return (1);
-}
-
-/**
- * _strlen - Calculates the length of a string.
- * @s: The string.
- *
- * Return: The length of the string.
- */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (s[len])
-		len++;
-
-	return (len);
-}
-
-/**
- * multiply - Multiplies two strings of numbers.
- * @num1: The first number as a string.
- * @num2: The second number as a string.
- *
- * Return: Pointer to the result as a string, or NULL on failure.
- */
-char *multiply(char *num1, char *num2)
-{
-	int len1 = _strlen(num1), len2 = _strlen(num2);
-	int len = len1 + len2;
-	int i, j, carry, digit1, digit2;
-	int *result = calloc(len, sizeof(int));
-	char *output;
-
-	if (result == NULL)
-	return (NULL);
-
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		digit1 = num1[i] - '0';
-		carry = 0;
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			digit2 = num2[j] - '0';
-			carry += result[i + j + 1] + (digit1 * digit2);
-			result[i + j + 1] = carry % 10;
-			carry /= 10;
-		}
-		result[i + j + 1] += carry;
-	}
-	output = malloc(len + 1);
-	if (output == NULL)
-	{
-		free(result);
-		return (NULL);
-	}
-
-	i = 0, j = 0;
-	while (i < len)
-	{
-		if (result[i] != 0 || j > 0)
-			output[j++] = result[i] + '0';
+		_putchar(str[i]);
 		i++;
 	}
-	output[j] = '\0';
-
-	free(result);
-	return (j > 0 ? output : "0");
 }
 
 /**
- * errors - Prints an error message and exits with status 98.
+ * _atoi - convert a string to an integer
+ * @s: char type string
+ * Return: integer converted
  */
-void errors(void)
+
+int _atoi(const char *s)
 {
-	printf("Error\n");
-	exit(98);
+	int sign = 1;
+	unsigned long int resp = 0, firstNum, i;
+
+	for (firstNum = 0; !(s[firstNum] >= '0' && s[firstNum] <= '9'); firstNum++)
+	{
+		if (s[firstNum] == '-')
+		{
+			sign *= -1;
+		}
+	}
+
+	for (i = firstNum; s[i] >= '0' && s[i] <= '9'; i++)
+	{
+		resp *= 10;
+		resp += (s[i] - '0');
+	}
+
+	return (sign * resp);
 }
 
 /**
- * main - Multiplies two positive numbers.
- * @argc: The number of arguments.
- * @argv: The argument vector.
- *
- * Return: 0 on success.
+ * print_int - prints an integer
+ * @n: int
+ * Return: void
  */
-int main(int argc, char *argv[])
-{
-	char *num1, *num2, *result;
 
+void print_int(unsigned long int n)
+{
+	unsigned long int divisor = 1, i, resp;
+
+	for (i = 0; n / divisor > 9; i++, divisor *= 10)
+		;
+
+	for (; divisor >= 1; n %= divisor, divisor /= 10)
+	{
+		resp = n / divisor;
+		_putchar('0' + resp);
+	}
+}
+
+/**
+ * main - prints the result of the multiplication, followed by a new line
+ * @argc: int
+ * @argv: list of arguments
+ * Return: 0 on success, 98 on error
+ */
+
+int main(int argc, char const *argv[])
+{
 	if (argc != 3)
-		errors();
+	{
+		_puts("Error");
+		exit(98);
+	}
 
-	num1 = argv[1];
-	num2 = argv[2];
-
-	if (!_isdigit(num1) || !_isdigit(num2))
-		errors();
-
-	result = multiply(num1, num2);
-	if (result == NULL)
-		errors();
-
-	printf("%s\n", result);
-	free(result);
+	print_int(_atoi(argv[1]) * _atoi(argv[2]));
+	_putchar('\n');
 
 	return (0);
 }
