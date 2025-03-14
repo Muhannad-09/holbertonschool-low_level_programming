f25ea55a34af4178b0c4637ef0ac40cf5ade2e8d
Muhannad-09 <muhannadgsgs@gmail.com>
2025-03-14 02:04:44 +0000

    101

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -65,11 +65,14 @@ int main(int argc, char *argv[])
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
+
 	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
+
 	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
+
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
@@ -84,6 +87,7 @@ int main(int argc, char *argv[])
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
 	}
+
 	for (i = 0; i < len - 1; i++)
 	{
 		if (result[i])
@@ -91,9 +95,12 @@ int main(int argc, char *argv[])
 		if (a)
 			_putchar(result[i] + '0');
 	}
+
 	if (!a)
 		_putchar('0');
+
 	_putchar('\n');
 	free(result);
+
 	return (0);
 }
