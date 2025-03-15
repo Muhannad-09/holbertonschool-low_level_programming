888f0a3b146c70618f8cce797e207acf0f8eca37
Muhannad-09 <muhannadgsgs@gmail.com>
2025-03-15 04:51:11 +0000

    101

--- more_malloc_free/launch_101-mul.sh
+++ more_malloc_free/launch_101-mul.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+# Compile the C files
 gcc -Wall -Werror -Wextra -pedantic -std=gnu89 101-mul.c _putchar.c -o 101-mul
+
+# Run the compiled program with the provided arguments
 ./101-mul "$@"
