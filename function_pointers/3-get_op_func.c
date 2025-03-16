08bf4ec714fca14e9b4c69bd123018cc44576ff2
Muhannad-09 <muhannadgsgs@gmail.com>
2025-03-16 11:29:45 +0000

    calc

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,11 +1,11 @@
 #include "3-calc.h"
-#include <stddef.h>  /* Include to define NULL */
+#include <stddef.h>  /* For NULL */
 
 /**
- * get_op_func - selects the correct function to perform the operation
- * @s: the operator
+ * get_op_func - Selects the correct function to perform the operation
+ * @s: The operator passed as argument to the program
  *
- * Return: a pointer to the function that corresponds to the operator
+ * Return: pointer to the function corresponding to the operator
  */
 int (*get_op_func(char *s))(int, int)
 {
@@ -19,17 +19,11 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	/* Check for invalid operator length */
-	if (s[1] != '\0')
-	{
-		return (NULL);  /* If there are multiple characters, it's an invalid operator */
-	}
-
-	/* Find the operator */
 	while (ops[i].op != NULL)
 	{
-		if (*s == *(ops[i].op))
+		if (*(ops[i].op) == *s)  /* Compare the operator with the input */
 			return (ops[i].f);
 		i++;
 	}
-	return
+	return (NULL);  /* Return NULL if no match is found */
+}
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -3,40 +3,51 @@
 #include "3-calc.h"
 
 /**
- * main - performs simple operations based on command line input
- * @argc: argument count
- * @argv: argument vector
+ * main - Performs simple operations
+ * @argc: The number of arguments
+ * @argv: The arguments array
  *
- * Return: Always 0.
+ * Return: 0 on success, other exit statuses on error
  */
 int main(int argc, char *argv[])
 {
-		int num1, num2;
-		int (*op_func)(int, int);
+	int num1, num2;
+	int (*op_func)(int, int);
 
-		if (argc != 4)
-		{
-				printf("Error\n");
-				exit(98);
-		}
+	/* Ensure correct number of arguments */
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[3]);
+	/* Convert arguments to integers */
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
-		op_func = get_op_func(argv[2]);
+	/* Check if the operator is valid */
+	if (argv[2][1] != '\0')  /* If more than one character is passed */
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-		if (op_func == NULL)
-		{
-				printf("Error\n");
-				exit(99);
-		}
+	/* Get the function for the operator */
+	op_func = get_op_func(argv[2]);
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-		if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
-		{
-				printf("Error\n");
-				exit(100);
-		}
+	/* Check for division by zero */
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-		printf("%d\n", op_func(num1, num2));
-		return (0);
+	/* Perform the operation and print the result */
+	printf("%d\n", op_func(num1, num2));
+	return (0);
 }
