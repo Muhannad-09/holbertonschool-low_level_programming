009c23173d8d43f49c57c55e76849303b7dc4e87
Muhannad-09 <muhannadgsgs@gmail.com>
2025-03-22 14:00:33 +0000

    asm

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,18 +1,17 @@
-section .data
-    hello db 'Hello, World',0xA  ; Define the string with a newline (0xA)
+global main
 
 section .text
-    global _start                ; Entry point for the program
+main:
+    mov rax, 1        ; syscall number for sys_write
+    mov rdi, 1        ; file descriptor 1 (stdout)
+    mov rsi, msg      ; pointer to the message
+    mov rdx, msglen   ; message length
+    syscall           ; invoke system call
 
-_start:
-    ; Write the string to stdout (file descriptor 1)
-    mov rax, 1                   ; syscall number for sys_write
-    mov rdi, 1                   ; file descriptor 1 (stdout)
-    mov rsi, hello               ; address of the string to print
-    mov rdx, 13                  ; length of the string (12 characters + 1 for newline)
-    syscall                      ; make the system call
+    mov rax, 60       ; syscall number for sys_exit
+    xor rdi, rdi      ; status 0 (EXIT_SUCCESS)
+    syscall           ; invoke system call
 
-    ; Exit the program
-    mov rax, 60                  ; syscall number for sys_exit
-    xor rdi, rdi                 ; return code 0
-    syscall                      ; make the system call
+section .data
+msg: db "Hello, World", 10
+msglen: equ $ - msg
